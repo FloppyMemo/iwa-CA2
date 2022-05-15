4ae0c39a6018f528355af213e67ffee2a9b5992f
FloppyMemo <caio.rocha1911@gmail.com>
2022-05-15 12:07:13 +0500

    added API

--- app.js
+++ app.js
@@ -22,64 +22,60 @@ mongoose.connect(db_url).then(()=>{
 
 //let students =[]// JSON.parse(fs.readFileSync("data.json")); // Getting all the students at once in the beginning...
 
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
   console.log("Ok")  
-//   const students=Student.find()
-//     res.send(students)
   res.render("index"); // displaying the all Students page
 });
 
-// app.get("/new", (req, res) => {
-//   res.render("new"); // displaying the new student page
-// });
+app.get("/new", (req, res) => {
+  res.render("new"); // displaying the new student page
+});
 
-// app.get("/students", (req, res) => {
+app.get("/students",(req, res) => {
 
-//     const students=Student.find();
-
-
-//     res.send(students); // sending data of all students in json format to client.
-// });
-// app.post("/students", (req, res) => {
-//   // accepts params as body and assigns ID to the student and adds the student to the students JSON object
-//   const student = req.body;
-//   console.log(student)
-//   students.students.push(student);
-//   len = students.students.length;
-//   if (len == 1) {
-//     students.students[len - 1].id = 1;
-//   } else {
-//     students.students[len - 1].id = students.students[len - 2].id + 1;
-//   }
-//   // Convert the students json object to string and overwrites it to data.json
-//   var jsonContent = JSON.stringify(students);
-//   fs.writeFile("data.json", jsonContent, "utf8", function (err) {
-//     if (err) {
-//       console.log("An error occured while writing JSON Object to File.");
-//       return res.send({ msg: "Failure!!" });
-//     }
-//     res.redirect("/");
-//   });
-// });
-// app.delete("/students", (req, res) => {
-//   // gets the user id and finds the user in the students json object
-//   id = req.body.id;
-//   for (var i in students.students) {
-//     if (students.students[i].id == id) {
-//       students.students.splice(i, 1); // deletes user upon finding
-//       // converts the students json object to string and overwrites it to data.json
-//       var jsonContent = JSON.stringify(students);
-//       fs.writeFile("data.json", jsonContent, "utf8", function (err) {
-//         if (err) {
-//           console.log("An error occured while writing JSON Object to File.");
-//           return res.send({ msg: "Failure!!" });
-//         }
-//       });
-//       res.send({ msg: "Success" });
-//       return;
-//     }
-//   }
-//   res.send({ msg: "Failure" });
-// });
+    Student.find().then(result=>res.send(result));
+    // res.send({students:students.json()}); // sending data of all students in json format to client.
+});
+app.post("/students", (req, res) => {
+  // accepts params as body and assigns ID to the student and adds the student to the students JSON object
+  const student = req.body;
+  console.log(student)
+  students.students.push(student);
+  len = students.students.length;
+  if (len == 1) {
+    students.students[len - 1].id = 1;
+  } else {
+    students.students[len - 1].id = students.students[len - 2].id + 1;
+  }
+  // Convert the students json object to string and overwrites it to data.json
+  var jsonContent = JSON.stringify(students);
+  fs.writeFile("data.json", jsonContent, "utf8", function (err) {
+    if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return res.send({ msg: "Failure!!" });
+    }
+    res.redirect("/");
+  });
+});
+app.delete("/students", (req, res) => {
+  // gets the user id and finds the user in the students json object
+  id = req.body.id;
+  for (var i in students.students) {
+    if (students.students[i].id == id) {
+      students.students.splice(i, 1); // deletes user upon finding
+      // converts the students json object to string and overwrites it to data.json
+      var jsonContent = JSON.stringify(students);
+      fs.writeFile("data.json", jsonContent, "utf8", function (err) {
+        if (err) {
+          console.log("An error occured while writing JSON Object to File.");
+          return res.send({ msg: "Failure!!" });
+        }
+      });
+      res.send({ msg: "Success" });
+      return;
+    }
+  }
+  res.send({ msg: "Failure" });
+});
 
 app.listen(port, () => console.log(`Listening on Port ${port}!!`));
